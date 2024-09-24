6474c889735381b7a30cf9dec4886771ad322435
darkoso7 <96436492+darkoso7@users.noreply.github.com>
2024-09-24 14:36:10 -0500

    Refactor data loading process to load data from classpath resources

--- transport-planning/src/main/java/com/odiparpack/transport_planning/TransportPlanningApplication.java
+++ transport-planning/src/main/java/com/odiparpack/transport_planning/TransportPlanningApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
 import com.odiparpack.transport_planning.service.DataLoaderService;
 import com.odiparpack.transport_planning.service.GLSAlgorithmService;
 
@@ -17,6 +20,9 @@ public class TransportPlanningApplication {
     @Autowired
     private GLSAlgorithmService glsAlgorithmService;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     public static void main(String[] args) {
         SpringApplication.run(TransportPlanningApplication.class, args);
     }
@@ -24,12 +30,13 @@ public class TransportPlanningApplication {
     @Bean
     public ApplicationRunner initializer() {
         return args -> {
-            // Load data
-            String citiesFilePath = "data/cities.txt";
-            String roadSegmentsFilePath = "data/road_segments.txt";
+            // Load data from classpath resources
+            Resource citiesFileResource = resourceLoader.getResource("classpath:data/cities.txt");
+            Resource roadSegmentsFileResource = resourceLoader.getResource("classpath:data/road_segments.txt");
 
-            dataLoaderService.loadCities(citiesFilePath);
-            dataLoaderService.loadRoadSegments(roadSegmentsFilePath);
+
+            dataLoaderService.loadCities(citiesFileResource.getFile().getPath());
+            dataLoaderService.loadRoadSegments(roadSegmentsFileResource.getFile().getPath());
             dataLoaderService.loadTrucksFromData();
             dataLoaderService.loadSalesData();
 
